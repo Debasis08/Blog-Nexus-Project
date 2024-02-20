168ef63fde7b34ba47c4f2a3b07061cadca1e8a5
Debasis08 <patrodebasis8@gmail.com>
2024-02-20 17:34:58 +0530

    Added TailwindCSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,30 @@
+import { useState } from 'react'
+import { useDispatch } from 'react-redux'
+import React, {useEffect} from 'react'
+import authService from "./appwrite/auth"
+import {login, logout} from "./store/authSlice"
 import './App.css'
 
 function App() {
 
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>Blog Nexus</h1>
-    </>
-  ) 
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData) => {
+      if (userData) {
+        dispatch(login({userData}))
+      } else {
+        dispatch (logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  }, [dispatch])
+
+  return !loading ? (
+    <div> </div>
+  ) : null
 }
 
 export default App
